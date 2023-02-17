97b7517a723ae07a455bde44ec17fd0d5cb5c0f3
wujiahao <1029172530@qq.com>
2023-02-17 15:58:06 +0800

    xiaohao自动上传到git

---
+++ push.sh
@@ -0,0 +1,33 @@
+###
+###
+ # @Author: xiaoHao
+### 
+ # @Author: xiaoHao
+### 
+
+path="F:/mime/HaozaiGo"
+
+cd $path
+
+echo "####### 进入自己的项目 #######"
+
+ls
+
+echo "开始执行命令"
+
+git add .
+git status
+sleep 1s
+
+echo "####### 添加文件 #######"
+
+git commit -m "xiaohao自动上传到git"
+
+echo "####### commit #######"
+
+sleep 1s
+
+echo "####### 开始推送 #######"
+git push
+
+echo "####### 推送成功 #######"
